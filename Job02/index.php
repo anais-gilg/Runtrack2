8063f25f6c52e4e1ec90e52ff8becaa2279fbf57
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-14 15:12:26 +0100

    Les commentaires
    Dans ce job02 nous découvrons les différentes manières de commenter son code php.
    Commentaire en ligne et commentaire en bloc

---
+++ Job02/index.php
@@ -0,0 +1,15 @@
+<?php 
+// Commentaire numéro 1 (commentaire en ligne)
+echo "Hello La Plateforme"; 
+
+# Commentaire numéro 2 (commentaire en ligne)
+echo "Hello La Plateforme";
+
+/* Ce commentaire est le même qu'en CSS
+et il permet de faire un commentaire 
+sur plusieurs lignes. 
+Dit aussi commaitaire en bloc
+
+Comme nous pouvons le remarquer
+actuellement  */
+?>
