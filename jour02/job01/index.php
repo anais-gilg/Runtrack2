c34d2f07b1290cce16bf2f9c9ecec3c76dccf684
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-15 12:51:37 +0100

    boucles et conditions php

---
+++ jour02/job01/index.php
@@ -0,0 +1,16 @@
+<?php 
+$i=0;
+
+for($i=0; $i<=1337; $i++){
+    if ($i == 42){
+        echo '<b><u>' .$i. '</b></u><br>'; // mettre des points autour de la variable comme effet point de colle 
+    }
+    else{
+        echo $i. '<br>';
+    }
+}
+
+
+
+
+?>
