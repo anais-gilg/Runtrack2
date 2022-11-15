4a4db289abda4e4b3c55101f162842208e833386
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-15 14:41:10 +0100

    != &&
    Afficher tous les nombres de 0 à 1337 compris SAUF 26, 37, 88, 1111 et 3233 en
    mettant un retour à la ligne entre chaque nombre (<br />).

---
+++ jour02/job02/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+/* Afficher tous les nombres de 0 à 1337 compris SAUF 26, 37, 88, 1111 et 3233 en
+mettant un retour à la ligne entre chaque nombre (<br />). */
+
+for($i = 0; $i <=1337; $i++)
+    if($i != 26 && $i != 37 && $i != 88 && $i != 1111) 
+        echo $i. '<br>'
+
+
+/* Si $i n'est pas égal à 26 et qu'il n'est pas égale à 37 etc... 
+   Afficher la suite avec un retour à la ligne */
+?>
