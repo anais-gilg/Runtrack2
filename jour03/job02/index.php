28a3091b5d4699a9b9ee79f42b59248dab761089
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-17 10:01:39 +0100

    afficher un caractère sur deux

---
+++ jour03/job02/index.php
@@ -0,0 +1,19 @@
+<?php 
+/* ---------------------------------------Consignes----------------------------------------------
+Créez une variable de type string nommée $str et affectez y le texte :
+“Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.”
+Parcourir cette chaîne en affichant seulement un caractère sur deux.
+-------------------------------------------------------------------------------------------------*/
+
+$str = 'Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.';
+
+
+for ($i=0; isset($str[$i]); $i++){
+    if ($i%2 == 0){ 
+        echo $str[$i];
+    }
+}
+
+
+
+?>
