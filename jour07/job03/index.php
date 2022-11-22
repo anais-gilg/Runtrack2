6e0e7a2389fcf2568c4a40e3b6058a0d6bc388ec
anais-gilg <114387786+anais-gilg@users.noreply.github.com>
2022-11-22 11:14:13 +0100

    return

---
+++ jour07/job03/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$hello = getHello(); 
+// Faire une variable avec comme valeur la fonction
+// Ce qui nous permettra de l'afficher par la suite
+
+function getHello(){
+    return 'Hello La Plateforme !';
+}
+
+echo $hello;
+    
+?>
